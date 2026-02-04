c2eac69e02c11a70aa17895cf87e56c3c381ae58
MVP2001 <mihailpodorets01@gmail.com>
2026-02-04 09:20:58 +0000

    Add HashiCorp Vault for secrets management

--- app/go.mod
+++ app/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.11
+	github.com/hashicorp/vault/api v1.22.0
 	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
@@ -11,7 +12,18 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.1.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.2.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
+	github.com/hashicorp/hcl v1.0.1-vault-7 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -19,15 +31,20 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.33.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
 )
--- app/internal/database/database.go
+++ app/internal/database/database.go
@@ -5,38 +5,58 @@ import (
 	"log"
 	"os"
 
+	"github.com/hashicorp/vault/api"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 func Connect() (*gorm.DB, error) {
-	// Получаем параметры подключения из env
-	host := getEnv("DB_HOST", "localhost")
-	user := getEnv("DB_USER", "postgres")
-	password := getEnv("DB_PASSWORD", "postgres")
-	dbname := getEnv("DB_NAME", "devops_app")
-	port := getEnv("DB_PORT", "5432")
+	// Пробуем получить из Vault
+	vaultAddr := os.Getenv("VAULT_ADDR")
+	vaultToken := os.Getenv("VAULT_TOKEN")
+	
+	var dbConfig map[string]interface{}
+	
+	if vaultAddr != "" && vaultToken != "" {
+		config := api.DefaultConfig()
+		config.Address = vaultAddr
+		
+		client, err := api.NewClient(config)
+		if err == nil {
+			client.SetToken(vaultToken)
+			
+			secret, err := client.KVv2("secret").Get(context.Background(), "app/database")
+			if err == nil && secret != nil {
+				dbConfig = secret.Data
+				log.Println("Loaded database config from Vault")
+			}
+		}
+	}
+	
+	// Fallback на env vars
+	host := getConfigValue(dbConfig, "DB_HOST", "DB_HOST", "localhost")
+	user := getConfigValue(dbConfig, "DB_USER", "DB_USER", "postgres")
+	password := getConfigValue(dbConfig, "DB_PASSWORD", "DB_PASSWORD", "postgres")
+	dbname := getConfigValue(dbConfig, "DB_NAME", "DB_NAME", "postgres")
+	port := getConfigValue(dbConfig, "DB_PORT", "DB_PORT", "5432")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		host, user, password, dbname, port)
 
-	log.Println("Connecting to database:", host)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
-	
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+func getConfigValue(vaultData map[string]interface{}, vaultKey, envKey, defaultVal string) string {
+	if vaultData != nil {
+		if val, ok := vaultData[vaultKey]; ok {
+			return fmt.Sprintf("%v", val)
+		}
 	}
-	return fallback
+	if val := os.Getenv(envKey); val != "" {
+		return val
+	}
+	return defaultVal
 }
